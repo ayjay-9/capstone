311af62e1f81c88eec8d8fb57c5e4213d6db130b
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-18 02:34:41 +0100

    add links to the run experiment page and view

--- capstone/ml_experiment_dashboard/urls.py
+++ capstone/ml_experiment_dashboard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('', views.index, name='index'),
     path('history/', views.history, name='history'),
+    path('experiment/<int:experiment_id>/', views.run_experiment, name='run_experiment'),
 ]
